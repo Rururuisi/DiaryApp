f6037091f9bc4927d4d4f5df6e563a01bca5ed48
Rururuisi <62675076+Rururuisi@users.noreply.github.com>
2023-08-24 21:06:53 -0400

    update journal

--- src/client/components/SettingList/UpdateJournal.js
+++ src/client/components/SettingList/UpdateJournal.js
@@ -7,15 +7,19 @@ function UpdateJournal() {
                 <b>目前进度：</b>
                 <br />
                 <br />
-                可注册登录退登注销账号,日记为用户私有，其他用户不可见;
-                已初步完成 Dashboard, Diary, Calendar 界面内容的开发;
+                可注册登录退登注销账号，日记为用户私有，其他用户不可见;
+                已初步完成 Dashboard，Diary，Calendar 界面内容的开发;
                 <br />
                 <br />
                 <b>-- Dashboard界面:</b> 日记总数与每月日记数统计图
                 <br />
                 <br />
-                <b>-- Diary界面:</b> 可创建、查看、修改、删除日记,
-                含有过滤器和根据创建时间排序功能, 可批量删除日记;
+                <b>-- Diary界面:</b> 可创建、查看、修改、删除日记，
+                含有过滤器和根据创建时间排序功能，可批量删除日记;
+                <br />
+                <small style={{ color: "red" }}>
+                    (目前已加入心情选择，图片添加功能正在开发中)
+                </small>
                 <br />
                 <br />
                 <b>-- Calendar界面:</b> 用花朵标记了含有日记内容的日期
@@ -31,8 +35,6 @@ function UpdateJournal() {
             <br />
             <p style={{ color: "blue" }}>
                 下一更新内容：
-                <br />
-                -- 加入日记心情选择;
                 <br />
                 -- 加入日记添加图片功能;
             </p>
